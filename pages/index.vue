bea1eac732e69b02e14655ae9f413555c4b00869
kiriha1203 <mizumoto.takefumi@gmail.com>
2021-06-15 03:33:55 +0900

    モジュールを使用したカウンターアプリ

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from '~/components/Counter.vue'
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+</style>
